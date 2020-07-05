f8bffde87b772922671ecc92b3c0c17328b7b695
caaarinschn <carina.weinhandl@student.tugraz.at>
2020-07-05 21:34:56 +0200

    🚧 login

--- public/pages/login.tsx
+++ public/pages/login.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
-export default class Index extends React.Component {
-    render() {
+interface IndexState {
+    errorMsg?: string;
+    cuError?: number;
+}
+
+export default class Index extends React.Component<void, IndexState> {
+    private userInput: HTMLInputElement;
+    private passInput: HTMLInputElement;
+
+    constructor (props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    private onLogin (e: FormEvent): void {
+        e.preventDefault();
+        const [uname, pwd] = [this.userInput.value.trim(), this.passInput.value];
+        if (!uname || !pwd) return this.showError('Please enter username and password!');
+        fetch('http://localhost:8080/users/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ uname, pwd, }),
+        }).then(res => res.json()).then(res => {
+            if (!res.success) return this.showError(res.msg);
+            localStorage.setItem('tkn', res.body.tkn);
+            console.log(JSON.parse(atob(localStorage.getItem('tkn').split('.')[1])).uname);
+            // TODO: logged-in stuff: redirect, etc.
+        });
+    }
+
+    private showError (msg: string): void {
+        if (this.state.cuError) window.clearTimeout(this.state.cuError);
+        this.setState({
+            errorMsg: msg,
+            cuError: window.setTimeout(() => this.setState({ errorMsg: undefined, }), 5000),
+        });
+    }
+
+    public render () {
         return (
             <>
                 <article>
@@ -12,16 +54,24 @@ export default class Index extends React.Component {
                     </div>
                     <div className="login">
                         <h1>Login</h1>
-                        <form>
-                            <input type="text" placeholder="Username" autoFocus />
-                            <input type="password" placeholder="●●●●●●●●●●●●●" />
+                        <form onSubmit={this.onLogin.bind(this)}>
+                            <input ref={e => this.userInput = e} type="text" placeholder="Username" autoFocus />
+                            <input ref={e => this.passInput = e} type="password" placeholder="●●●●●●●●●●●●●" />
                             <input type="submit" value="Login" />
                         </form>
                         <p>Not a member? <Link href="/register">Register</Link></p>
                     </div>
+                    <div className={'error ' + (this.state.errorMsg ? 'error-up' : 'error-down')}><i><FontAwesomeIcon icon={faExclamationCircle} /></i> {this.state.errorMsg}</div>
                 </article>
 
                 <style jsx>{`
+                    article {
+                        position: relative;
+                        overflow: hidden;
+                        width: 100%;
+                        height: 100%;
+                    }
+
                     form {
                         display: flex;
                         flex-direction: column;
@@ -31,13 +81,14 @@ export default class Index extends React.Component {
                         margin: auto auto;
                         padding: 0;
                     }
-
+                    
                     input {
                         font-size: 17px;
                         color: #273036;
                         border: none;
                         border-bottom: 1px solid #DADADA;
                         transition-duration: 0.2s;
+                        background: none;
                     }
 
                     input:focus {
@@ -91,6 +142,33 @@ export default class Index extends React.Component {
                         top: -10%;
                         font-size: 30px;
                         margin-bottom: 30px;
+                    }
+
+                    @keyframes wiggle {
+                         0% { transform: rotate( 0deg); }
+                        25% { transform: rotate( 1.5deg); }
+                        50% { transform: rotate( 0deg); }
+                        75% { transform: rotate(-1.5deg); }
+                    }
+
+                    .error-down {
+                        transform: translateY(200%);
+                    }
+
+                    .error-up {
+                        animation: wiggle .2s linear .3s 1 forwards;
+                    }
+                    
+                    .error {
+                        position: absolute;
+                        bottom: 10px;
+                        right: 10px;
+                        background: rgba(255, 53, 96, .3);
+                        color: #E33157;
+                        border: 2px solid #FF3560;
+                        border-radius: 15px;
+                        padding: 10px;
+                        transition: .3s cubic-bezier(.25, .1, .3, 1.5);
                     }
                 `}</style>
             </>
